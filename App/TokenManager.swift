acb99755802583c3e85759112f8b1560b4174f35
colin0252 <zkn12306@outlook.com>
2026-07-13 19:34:01 +0800

    更新 TokenManager.swift

--- App/TokenManager.swift
+++ App/TokenManager.swift
@@ -4,20 +4,20 @@ class TokenManager: ObservableObject {
     @Published var savedToken = ""
     @Published var pendingAuth = false
     @Published var callbackScheme = ""
-    
+
     private let tokenKey = "com.delta.login.token"
-    
+
     init() { loadToken() }
-    
+
     func saveToken(_ token: String) {
         savedToken = token
         UserDefaults.standard.set(token, forKey: tokenKey)
     }
-    
+
     func loadToken() {
         savedToken = UserDefaults.standard.string(forKey: tokenKey) ?? ""
     }
-    
+
     func clearToken() {
         savedToken = ""
         UserDefaults.standard.removeObject(forKey: tokenKey)
